d5e9fd903b15bf784e60d434bbbd9289a1f7d8ca
am43malik <91952873+am43malik@users.noreply.github.com>
2024-05-21 12:37:52 +0530

    storechanges

--- models/Stock.js
+++ models/Stock.js
@@ -16,11 +16,16 @@
 
 const mongoose = require("mongoose")
 const stockScheme = new mongoose.Schema({
-    name:{type:String,default:"",required:true},
+    name:{type:String,default:"",},
     department:{type:String},
     product:{type:mongoose.Types.ObjectId,ref:"Product"},
-    totalQuantity:{type:Number,default:0, required:true},
+    totalQuantity:{type:Number,default:0, },
+    start:{type:String,default:"",},
+    end:{type:String,default:"",},
+    startColor:{type:String,default:"",},
+    endColor:{type:String,default:"",},
     expiryArray:[],
+
     stockIn:[{type:mongoose.Types.ObjectId,ref:"StockIn"}],
     stockOut:[{type:mongoose.Types.ObjectId,ref:"StockOut"}],
     Discard:[{type:mongoose.Types.ObjectId,ref:"Discard"}],
